2d6ef3692d719f8621fc29ee0cc783fce722df43
isleap9 <265657801+isleap9@users.noreply.github.com>
2026-08-13 19:38:43 +0200

    fixed AkariOS power plan

--- Executables/powerplan.bat
+++ Executables/powerplan.bat
@@ -7,13 +7,13 @@ set "DEVICE_TYPE=PC"
 for %%a in (8 9 10 11 12 13 14 18 21 30 31 32) do if "%CHASSIS%"=="%%a" set "DEVICE_TYPE=LAPTOP"
 
 if /I "%DEVICE_TYPE%"=="LAPTOP" (
-    powercfg /setactive 381b4222-f694-41f0-9685-ff5bb260df2e
+    powercfg /setactive d90bd747-9a4a-44ac-b19a-661a723534f5
     powercfg /setacvalueindex scheme_current 54533251-82be-4824-96c1-47b60b740d00 4d2b0152-7d5c-498b-88e2-34345392a2c5 5000
     powercfg /setdcvalueindex scheme_current 54533251-82be-4824-96c1-47b60b740d00 4d2b0152-7d5c-498b-88e2-34345392a2c5 5000
     cls
 ) else (
-    powercfg -import %SYSTEMROOT%\AkariOS.pow 8b1e6fbd-33a2-411c-9317-232e23154cf5
-    powercfg -setactive 8b1e6fbd-33a2-411c-9317-232e23154cf5
+    powercfg -import %SYSTEMROOT%\AkariOS.pow d90bd747-9a4a-44ac-b19a-661a723534f5
+    powercfg -setactive d90bd747-9a4a-44ac-b19a-661a723534f5 
     del %SYSTEMROOT%\AkariOS.pow
     cls
 )
